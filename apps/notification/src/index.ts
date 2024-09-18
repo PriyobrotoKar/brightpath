fd6046b68105bf18e8cadb0533cdd5bd7fbe47b0
Priyobroto Kar <priyobrotokar@gmail.com>
2024-09-18 22:01:54 +0530

    feat(app/notification): add the kafka consumer and push the event to redis queue

--- apps/notification/src/index.ts
+++ apps/notification/src/index.ts
@@ -1,10 +1,21 @@
-import { KafkaClient } from '@brightpath/kafka';
+import { BullMQClient } from '@brightpath/bullmq';
+import {
+  KafkaClient,
+  transactionNotificationMessageSchema,
+} from '@brightpath/kafka';
 
 const kafkaClient = new KafkaClient('notification', ['localhost:9092']);
 
-kafkaClient.startConsumer(['transaction-notification'], ({ value }) => {
+kafkaClient.startConsumer(['transaction-notification'], async ({ value }) => {
   if (!value) {
     return;
   }
+  const message = transactionNotificationMessageSchema.parse(
+    JSON.parse(value.toString()),
+  );
   console.log('Message received', JSON.parse(value.toString()));
+
+  const queue = new BullMQClient('Mail Queue');
+
+  await queue.addJob(message);
 });
