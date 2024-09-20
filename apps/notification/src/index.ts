a84353105643de8ae03d592b4634ec620af1cd82
Priyobroto Kar <priyobrotokar@gmail.com>
2024-09-21 00:22:16 +0530

    feat(notification): Update dev script in package.json and add sending events to the email queue based on priority

--- apps/notification/src/index.ts
+++ apps/notification/src/index.ts
@@ -6,6 +6,17 @@ import {
 
 const kafkaClient = new KafkaClient('notification', ['localhost:9092']);
 
+const eventTypes = transactionNotificationMessageSchema.options.map(
+  (option) => option.shape.eventType.value,
+);
+
+type Event = (typeof eventTypes)[number];
+
+const priorities: Record<Event, number> = {
+  email_verification: 0,
+  account_creation: 1,
+};
+
 kafkaClient.startConsumer(['transaction-notification'], async ({ value }) => {
   if (!value) {
     return;
@@ -17,5 +28,5 @@ kafkaClient.startConsumer(['transaction-notification'], async ({ value }) => {
 
   const queue = new BullMQClient('Mail Queue');
 
-  await queue.addJob(message);
+  await queue.addJob(message, priorities[message.eventType]);
 });
