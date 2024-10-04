b67c751f581978b337d44d11b965e1034b00a636
Priyobroto Kar <priyobrotokar@gmail.com>
2024-10-04 14:03:00 +0530

    feat(ui): Remove unused code and update dependencies

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -19,6 +19,41 @@ const config = {
         '2xl': '1400px',
       },
     },
+    fontSize: {
+      xs: [
+        '0.75rem',
+        {
+          lineHeight: '0.875rem',
+        },
+      ],
+      sm: [
+        '0.875rem',
+        {
+          lineHeight: '0.875rem',
+          fontWeight: '500',
+        },
+      ],
+      'md-semibold': [
+        '0.875rem',
+        {
+          lineHeight: '1.25rem',
+          fontWeight: '500',
+        },
+      ],
+      md: [
+        '0.875rem',
+        {
+          lineHeight: '1.25rem',
+          fontWeight: '400',
+        },
+      ],
+      base: ['1rem', { lineHeight: '1.5rem', fontWeight: '400' }],
+      'base-medium': ['1rem', { lineHeight: '1.5rem', fontWeight: '500' }],
+      lg: ['1.125rem', { lineHeight: '1.75rem', fontWeight: '400' }],
+      xl: ['1.5rem', { lineHeight: '2rem', fontWeight: '600' }],
+      '2xl': ['1.875rem', { lineHeight: '2.25rem', fontWeight: '600' }],
+      '3xl': ['3rem', { lineHeight: '3rem', fontWeight: '800' }],
+    },
     extend: {
       colors: {
         border: 'hsl(var(--border))',
