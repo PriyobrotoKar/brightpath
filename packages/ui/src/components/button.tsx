c6df9e67567ae5e0e8dbabfeed77085b45cf542d
Priyobroto Kar <priyobrotokar@gmail.com>
2024-10-21 02:05:48 +0530

    chore(ui): Update dependencies and styles
    Update package.json to include new dependencies: @radix-ui/react-alert-dialog and @radix-ui/react-avatar.
    Update globals.css to adjust color variables.
    Update input.tsx to import modules using single quotes and adjust class names.
    Add new component avatar.tsx.
    Update button.tsx to adjust button variants.

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -7,17 +7,18 @@ import { cn } from '@brightpath/ui/lib/utils';
 import { Loader } from 'lucide-react';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-60',
+  'inline-flex  items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-60',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        default:
+          'bg-primary shadow-sm text-primary-foreground hover:bg-primary/90',
         destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+          'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
         outline:
-          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
+          'border border-input bg-background hover:bg-accent shadow-sm hover:text-accent-foreground',
         secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+          'bg-secondary text-secondary-foreground hover:bg-secondary/80 shadow-sm',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
