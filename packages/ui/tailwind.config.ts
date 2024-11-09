a9cbd1ed46c24bc0b7152f7c2f4dd6bdaf789d74
Priyobroto Kar <priyobrotokar@gmail.com>
2024-11-10 01:52:48 +0530

    feat(ui): Add Popover and Dialog components with styling and functionality enhancements

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -116,7 +116,7 @@ const config = {
       },
     },
   },
-  plugins: [tailwindcssAnimate],
+  plugins: [tailwindcssAnimate, require('@tailwindcss/typography')],
 } satisfies Config;
 
 export default config;
