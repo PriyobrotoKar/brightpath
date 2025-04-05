07e043b92370ae00393709d1ac5b1060e23b053a
Priyobroto Kar <priyobrotokar@gmail.com>
2025-04-06 00:00:09 +0530

    feat(redis): update redis to accept env variables

--- packages/redis/src/index.ts
+++ packages/redis/src/index.ts
@@ -1,14 +1,25 @@
+import { config } from 'dotenv';
 import ioredis, { RedisOptions } from 'ioredis';
 
+config({
+  path: `../../.env${process.env.NODE_ENV ? '.' + process.env.NODE_ENV : ''}`,
+});
+
+const port = process.env.REDIS_PORT ?? 6379;
+const host = process.env.REDIS_HOST ?? 'localhost';
+
 const redisConfig: RedisOptions = {
-  port: 6379,
-  host: 'localhost',
-  maxRetriesPerRequest: null,
+  port: Number(port),
+  host,
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  ...(!!process.env.REDIS_HOST && { tls: {} }),
 };
 
 const redisConnection = new ioredis(redisConfig);
 
-redisConnection.on('error', () => {
+redisConnection.on('error', (e) => {
+  console.log(e);
   console.log('Error in Redis Connection');
 });
 
