9ecafb49f2673cc41ebebc547c779becd96578f2
Priyobroto Kar <priyobrotokar@gmail.com>
2025-04-12 00:32:37 +0530

    feat(db): create a db connection pool for serverless

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,1 +1,10 @@
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = globalThis as typeof globalThis & {
+  prisma?: PrismaClient;
+};
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 export * from '@prisma/client';
+
+globalForPrisma.prisma = prisma;
